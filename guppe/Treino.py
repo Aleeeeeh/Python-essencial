e19500617ef375557f76c17aa1be6629b3c218d9
Aleeeeeh <alefesilva097@gmail.com>
2022-06-11 17:47:01 -0300

    Python

--- guppe/Treino.py
+++ guppe/Treino.py
@@ -20,7 +20,33 @@ Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
+**************************************************************************
+Tradução:
+
+O Zen de Python, de Tim Peters
+
+Bonito é melhor que feio.
+Explícito é melhor que implícito.
+Simples é melhor que complexo.
+Complexo é melhor do que complicado.
+Plano é melhor do que aninhado.
+Esparso é melhor do que denso.
+A legibilidade conta.
+Casos especiais não são especiais o suficiente para quebrar as regras.
+Embora a praticidade supere a pureza.
+Os erros nunca devem passar silenciosamente.
+A menos que explicitamente silenciado.
+Diante da ambiguidade, recuse a tentação de adivinhar.
+Deve haver uma - e de preferência apenas uma - maneira óbvia de fazer isso.
+Embora essa maneira possa não ser óbvia no início, a menos que você seja holandês.
+Agora é melhor do que nunca.
+Embora nunca seja melhor do que *agora*.
+Se a implementação é difícil de explicar, é uma má ideia.
+Se a implementação for fácil de explicar, pode ser uma boa ideia.
+Namespaces são uma ótima ideia -- vamos fazer mais desses!
 """
+
+
 
 
 
